7c8906a453c2c42818a0387bf78efe082679aaa2
Hugo Huxley <datalogado@gmail.com>
2026-09-21 10:19:24 -0300

    Delete Exercícios da Aula 01 directory

--- Exercícios da Aula 01/7. Calculadora de Multiplicação/script.js
+++
@@ -1,8 +0,0 @@
-function multiplicar(){
-    let num1 = document.getElementById("numero_1").value;
-    let num2 = document.getElementById("numero_2").value;
-
-    let resultado = num1 * num2;
-
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado; 
-}
